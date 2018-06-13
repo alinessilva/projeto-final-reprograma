43dc1c164fe9f783edd32ea6974a87aa980d7306
alinessilva <alinedoze@gmail.com>
2018-06-12 23:18:57 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Search from './Components/Search/Search';
+import './App.css';
+import Result from './Components/Result/Result';
+
+class App extends Component {
+  render() {
+    return (
+     <div>
+       <Search />
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Search/Search.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import Result from '../Result/Result'
+import './search.css'
+
+
+class Search extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            results:[],
+            newInput: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+    handleSubmit (e) {
+        this.setState({
+            results: [
+                this.state.results,
+                { text: this.state.newInput }
+            ]
+        });
+
+        this.setState({ newInput: '' })
+        e.preventDefault()
+        
+    }
+
+    handleTextChange (e) {
+        this.setState({ newInput: e.target.value })
+    }
+
+    // handleChange = (e) => {
+        
+    //     const valorDoInput = e.target.value;
+        
+    //     console.log(valorDoInput)
+    // }
+
+    // handleClick = (e) => {
+    //     e.preventDefault();
+        
+    //     console.log('clicou')
+    // }
+
+        render() {
+            return(
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input className="input" value={this.state.newInput} onChange={this.handleTextChange} placeholder="Digite sua busca..." />
+                        <button type="submit">Pesquisar</button>
+                    </form>
+                    {this.state.results.map((result, index) => {
+                        return <Result key={index} text={result.text} />
+                    })}
+                </div>
+            )
+        }     
+}
+
+export default Search
