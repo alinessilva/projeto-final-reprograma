b0cb95fb55b30a10fce719632a4ca3507e89fbfc
camilaibs <camilaibs@gmail.com>
2018-06-18 22:13:29 -0300

    Ajuda na listagem de repositórios

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,37 +1,19 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import avatar from './avatar.jpg'
 
 class Card extends Component {
-  constructor(props) {
-    super(props)
-    this.state = [
-      {
-        image: '',
-        name: 'Cynthia Zanoni',
-        repository: 'wifi-livre-sp',
-        description: 'Lista de locais públicos em São Paulo (capital) com acesso livre à internet'
-      },
-      {
-        image: '',
-        name: 'Aline Silva',
-        repository: 'Diamond',
-        description: 'Lorem Ipsum'
-      }
-    ]
-  }
-
   handleClick = (e) => {
     e.preventDefault()
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <img src={avatar} alt="Avatar"/>
-        <h3>{this.state.name}</h3>
-        <p>{this.state.repository}</p>
-        <p>{this.state.description}</p>
-        <a href="#" onClick={this.handleClick}>Link</a>    
+        <img src={this.props.image || avatar} alt="Avatar" />
+        <h3>{this.props.name}</h3>
+        <p>{this.props.repository}</p>
+        <p>{this.props.description}</p>
+        <a nClick={this.handleClick}>Link</a>
       </div>
     )
   }
