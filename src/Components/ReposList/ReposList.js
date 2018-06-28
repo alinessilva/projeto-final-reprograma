9da77e88ee0fb5d8fe0fe24a403c5c0843c0c9a9
alinessilva <alinedoze@gmail.com>
2018-06-28 00:39:07 -0300

    Add busca na API por Repositório

--- src/Components/ReposList/ReposList.js
+++ src/Components/ReposList/ReposList.js
@@ -4,24 +4,26 @@ import './ReposList.css'
 
 const ReposList = props => {
     return(
-        <ul>
-       {props.repos.map(repo => (
-           <li key={ repo.id }>{ repo.name } </li>
-       ))}
-   </ul>)
-    // <div className="position">    
-    //     {props.users.map(user => (
-    //         <Card className="card-input">
-    //             <Row className="row">
-    //                 <Col className="collum">
-    //                     <img className="responsive-img" alt="avatar" src={ user.avatar_url } />
-    //                     <li>{ props.user.name }</li>
-    //                     <li>{ user.login }</li>
-    //                 </Col>      
-    //             </Row>
-    //         </Card>
-    //     ))}
-    // </div>)
+    <div className="position">    
+        {props.users.map(user => (
+            <Card className="card-input">
+                <Row key={user.id} className="row">
+                    <Col className="collum">
+                        <img className="responsive-img" alt="avatar" src={ user.avatar_url } />
+                        <li>{ props.user.name }</li>
+                        <ul>
+                            {props.repos.map(repo => (
+                                <li key={ repo.id }>{ repo.name } </li>
+                            ))}
+                        </ul>
+                        <li>{ user.login }</li>
+                    </Col>      
+                </Row>
+            </Card>
+        ))}
+    </div>)
 }
 
 export default ReposList
+
+
